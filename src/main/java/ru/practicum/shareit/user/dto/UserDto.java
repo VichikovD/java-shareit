5e3f3cc242934dc590d1a790b5d18457450c7b95
VichikovD <131587393+VichikovD@users.noreply.github.com>
2023-11-28 19:06:29 +0300

    ShareIt 0.1.2 (add-controllers final)
    feat: working api with operations: CRUD users, CRUD items; Exception handling; double hashMap by different key for faster search

---
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -0,0 +1,24 @@
+package ru.practicum.shareit.user.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import ru.practicum.shareit.groupMarker.OnCreate;
+import ru.practicum.shareit.groupMarker.OnUpdate;
+import ru.practicum.shareit.validation.NotEmptyIfNotNull;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+public class UserDto {
+    private Long id;
+
+    @Email(groups = {OnCreate.class, OnUpdate.class}, message = "Invalid user's email")
+    @NotBlank(groups = OnCreate.class, message = "Invalid user's email. It should not be null or empty")
+    private String email;
+
+    @NotBlank(groups = OnCreate.class, message = "Invalid user's name. It should not be null or empty")
+    @NotEmptyIfNotNull(groups = OnUpdate.class)
+    private String name;
+}
